5da630fa7ff6d36b3b12d6e513b2f781c3618d27
Quyu Kong <kongquyu@gmail.com>
2019-11-09 13:54:50 +1100

    Create package.R

---
+++ R/package.R
@@ -0,0 +1,9 @@
+# include all required packages 
+
+  library(parallel)
+  library(pbmcapply)
+  library(tidyverse)
+  library(data.table)
+  library(magrittr)
+  library(cowplot)
+  theme_set(theme_cowplot())
